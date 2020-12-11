b66580dccaa9a5454f91c2eba14e336f07c1ba70
Jeff-wu-hub <15809812381@163.com>
2020-12-11 17:14:43 +0800

    上传例题

--- leetcode/editor/cn/[35]搜索插入位置.js
+++ leetcode/editor/cn/[35]搜索插入位置.js
@@ -1,31 +1,31 @@
-//给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。 
+//给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
 //
-// 你可以假设数组中无重复元素。 
+// 你可以假设数组中无重复元素。
 //
-// 示例 1: 
+// 示例 1:
 //
 // 输入: [1,3,5,6], 5
 //输出: 2
-// 
 //
-// 示例 2: 
+//
+// 示例 2:
 //
 // 输入: [1,3,5,6], 2
 //输出: 1
-// 
 //
-// 示例 3: 
+//
+// 示例 3:
 //
 // 输入: [1,3,5,6], 7
 //输出: 4
-// 
 //
-// 示例 4: 
+//
+// 示例 4:
 //
 // 输入: [1,3,5,6], 0
 //输出: 0
-// 
-// Related Topics 数组 二分查找 
+//
+// Related Topics 数组 二分查找
 // 👍 757 👎 0
 
 
@@ -36,6 +36,18 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-
+    let lo = 0;
+    let hi = nums.length - 1;
+    while (lo <= hi) {
+        const mid = (lo + hi) >>> 1;
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            hi = mid - 1;
+        } else {
+            lo = mid + 1;
+        }
+    }
+    return lo; // 退出循环时 hi比lo小1
 };
 //leetcode submit region end(Prohibit modification and deletion)
