6da0b21284f267f8baad7c93104b50edaca30ed1
Jeff-wu-hub <15809812381@163.com>
2020-12-15 10:34:50 +0800

    上传

--- leetcode/editor/cn/[58]最后一个单词的长度.js
+++ leetcode/editor/cn/[58]最后一个单词的长度.js
@@ -20,10 +20,19 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLastWord = function(s) {
-    let index = s.length -1
-    for(let i = s.length-1; i>=0;i--){
+var addBinary = function(a, b) {
 
+    let ans = ""; // 返回的字符串
+    let ca = 0; // 进位职位1或0
+    for(let i = a.length - 1, j = b.length - 1;i >= 0 || j >= 0; i--, j--) { // 两个都结束推出
+        let tempA = !a[i]? 0:parseInt(a[i])
+        let tempB = !b[j]? 0:parseInt(b[j])
+        let tempC = tempA + tempB + ca
+        ca = tempC>=2? 1:0
+        ans+=tempC%2
     }
+    ans += ca === 1 ? ca : ""; // 判断最后一位是否进位？
+    return ans.split('').reverse().join('');  // 由于字符串向后加，所以需要取反，先转换为数组=>取反=>转换字符串
 };
+console.log(addBinary('11','1'))
 //leetcode submit region end(Prohibit modification and deletion)
