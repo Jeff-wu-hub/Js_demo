c5cc0932a33b7474cad7d9026fd1d0d59ce8af8b
Jeff-wu-hub <15809812381@163.com>
2020-12-15 17:28:28 +0800

    删除排序列表重复项目，简单在于链表是排序的，使用链表指针即可完成

--- leetcode/editor/cn/[83]删除排序链表中的重复元素.js
+++ leetcode/editor/cn/[83]删除排序链表中的重复元素.js
@@ -1,16 +1,16 @@
-//给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。 
+//给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
 //
-// 示例 1: 
+// 示例 1:
 //
 // 输入: 1->1->2
 //输出: 1->2
-// 
 //
-// 示例 2: 
+//
+// 示例 2:
 //
 // 输入: 1->1->2->3->3
-//输出: 1->2->3 
-// Related Topics 链表 
+//输出: 1->2->3
+// Related Topics 链表
 // 👍 438 👎 0
 
 
@@ -27,6 +27,13 @@
  * @return {ListNode}
  */
 var deleteDuplicates = function(head) {
-
+    let res = head
+    while(res && res.next){
+        if(res.val == res.next.val){
+            res.next = res.next.next
+        }
+        else res = res.next
+    }
+    return head
 };
 //leetcode submit region end(Prohibit modification and deletion)
