6ec6ad0fc4361a587ab2a72b09233e3dcaf03104
Jeff-wu-hub <15809812381@163.com>
2020-12-15 15:34:28 +0800

    上传X的平方根

--- leetcode/editor/cn/[69]x 的平方根.js
+++ leetcode/editor/cn/[69]x 的平方根.js
@@ -27,7 +27,20 @@
  * @param {number} x
  * @return {number}
  */
-var mySqrt = function(x) {
-
-};
+var mySqrt = function (x) {
+    if (x == 0 || x == 1) {
+        return x;
+    }
+    let left = 1;
+    let right = x;
+    while (right - left > 1) {
+        let mid = Math.floor((right + left) /2 );
+        if (mid * mid > x) {
+            right = mid;
+        } else {
+            left = mid;
+        }
+    }
+    return left;
+}
 //leetcode submit region end(Prohibit modification and deletion)
